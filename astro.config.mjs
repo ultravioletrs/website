63b5e34e6cd2224de366d9f9bd82b071a9dc84a5
Jilks Smith <smithjilks@gmail.com>
2026-03-27 17:14:02 +0300

    Remove comments
    Signed-off-by: Jilks Smith <smithjilks@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,20 +16,6 @@ export default defineConfig({
       changefreq: "hourly",
       priority: 1.0,
       serialize(item) {
-        // Example: Get last modified date from frontmatter for content collections
-        // let modifiedTime = item.frontmatter?.lastModified;
-
-        // Example: If you have a custom function to get the date
-        // const urlPath = new URL(item.url).pathname;
-        // let modifiedTime = getLastModDate(urlPath);
-
-        // If a valid date is found, add it to the sitemap item
-        // The value should be an ISO formatted date string
-        // if (modifiedTime) {
-        //   item.lastmod = new Date(modifiedTime).toISOString();
-        // }
-
-        // For a simple, site-wide lastmod (less ideal for fresh content, but works)
         item.lastmod = new Date().toISOString(); // Sets all pages to the build time
 
         return item;
