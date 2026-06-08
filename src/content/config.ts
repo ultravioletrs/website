254be452f04c14837c74b7e72d0cbdca0b6dcc5a
ianmuchyri <ianmuchiri8@gmail.com>
2026-06-08 10:38:00 +0300

    simplify solutions and industries
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,13 +23,39 @@ const blog = defineCollection({
   }),
 });
 
-const industries = defineCollection({
-  type: "content",
-  schema: z.object({ title: z.string() }),
+const supportingProduct = z.object({
+  which: z.string(),
+  name: z.string(),
+  role: z.string(),
+  blurb: z.string(),
 });
-const solutions = defineCollection({
-  type: "content",
-  schema: z.object({ title: z.string() }),
+
+const lensPageSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  eyebrow: z.string(),
+  h1: z.string(),
+  sub: z.string(),
+  compH2: z.string(),
+  compBody: z.string(),
+  compHeaders: z.tuple([z.string(), z.string()]),
+  compRows: z.array(z.tuple([z.string(), z.string(), z.string()])),
+  product: z.object({
+    which: z.string(),
+    name: z.string(),
+    role: z.string(),
+    blurb: z.string(),
+    points: z.array(z.string()),
+    supporting: z.array(supportingProduct).optional(),
+  }),
+  ctaH2: z.string(),
+  ctaSub: z.string(),
+  ctaLabel: z.string(),
+  ctaHref: z.string(),
+  ctaExternal: z.boolean().optional(),
 });
+
+const industries = defineCollection({ type: "data", schema: lensPageSchema });
+const solutions = defineCollection({ type: "data", schema: lensPageSchema });
 
 export const collections = { blog, industries, solutions };
